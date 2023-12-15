77aa1c78f6ab78fb62e925d677c92a7ecedb2772
layorschou <layorschou@oringnet.com>
2023-12-15 18:55:07 +0800

    feat(excel): add read excel

--- src/modules/excel/excel.service.ts
+++ src/modules/excel/excel.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { read } from 'xlsx';
+import { read, utils } from 'xlsx';
 
 export const EXCEL_SERVICE = Symbol('EXCEL_SERVICE');
 
@@ -8,5 +8,14 @@ export class ExcelService {
   async transform(file: Express.Multer.File): Promise<Buffer> {
     // write your code for transforming
     return;
+  }
+
+  async read(fileBuffer: Buffer): Promise<any[]> {
+    const workbook = read(fileBuffer);
+    // customize your sheet name here
+    const sheetName = workbook.SheetNames[0];
+    const workSheet = workbook.Sheets[sheetName];
+    const rawData = utils.sheet_to_json(workSheet, { header: 2 });
+    return rawData;
   }
 }
